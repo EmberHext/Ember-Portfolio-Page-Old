acf414491f9bf4e4356c0a76a619f8fbafe99c29
Ember Hext <emberhext@gmail.com>
2023-04-10 15:02:36 -0400

    Update About.tsx

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faCss3, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faCss3, faJs, faReact, faLinux, faRust } from '@fortawesome/free-brands-svg-icons';
 
 const About: React.FC = () => {
   return (
@@ -15,12 +15,28 @@ const About: React.FC = () => {
           </p>
         </div>
         <div className="w-full md:w-auto flex flex-wrap items-center justify-center border-2 border-red-500 rounded-lg p-8 bg-gray-900">
-          <h2 className="text-3xl font-bold mb-4 w-full">Technologies</h2>
+          <h2 className="text-3xl font-bold mb-4 w-full">Languages and Technologies</h2>
           {/* Add technology icons here */}
-          <FontAwesomeIcon icon={faHtml5} className="text-4xl m-2" />
-          <FontAwesomeIcon icon={faCss3} className="text-4xl m-2" />
-          <FontAwesomeIcon icon={faJs} className="text-4xl m-2" />
-          <FontAwesomeIcon icon={faReact} className="text-4xl m-2" />
+          <img src="https://skillicons.dev/icons?i=rust" alt="Rust" className="text-8xl m-1"  />
+          <img src="https://skillicons.dev/icons?i=typescript" alt="TypeScript" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=nextjs" alt="Next JS" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=react" alt="React" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=express" alt="Express" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=nodejs" alt="NodeJS" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=tailwind" alt="Tailwind" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=php" alt="PHP" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=vue" alt="Vue" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=solidjs" alt="Solid JS" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=docker" alt="Docker" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=linux" alt="Linux" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=svelte" alt="Svelte" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=prisma" alt="Prisma" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=actix" alt="Actix" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=haskell" alt="Haskell" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=cpp" alt="C++" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=neovim" alt="Neovim" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=vscode" alt="VSCode" className="text-8xl m-1" />
+          <img src="https://skillicons.dev/icons?i=unreal" alt="Unreal" className="text-8xl m-1" />
           {/* Add more icons as needed */}
         </div>
       </div>

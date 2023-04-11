1f7865d544fccc3dbc4c79b6f1edb4d9721ead69
Ember Hext <emberhext@gmail.com>
2023-04-11 10:49:11 -0400

    Axum started

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,29 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
-    let cors = CorsLayer::new().allow_origin(Any);
+    // Route all requests on "/" endpoint to anonymous handler.
+    //
+    // A handler is an async function which returns something that implements
+    // `axum::response::IntoResponse`.
 
-    let app = Router::new()
-        .route("/", get(root))
-        .layer(cors);
+    // A closure or a function can be used as handler.
 
+    let app = Router::new().route("/", get(handler));
+    //        Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    // Address that server will bind to.
     let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
-    println!("listening on {}", addr);
 
+    // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app.
     axum::Server::bind(&addr)
+        // Hyper server takes a make service.
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn handler() -> &'static str {
+    "Hello, world!"
 }

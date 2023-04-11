29594fcdcb92fa0725ad4f725f08f0a10d4c7c79
Ember Hext <emberhext@gmail.com>
2023-04-11 10:12:23 -0400

    added some visual flair

--- client/src/components/ContactForm.tsx
+++ client/src/components/ContactForm.tsx
@@ -24,79 +24,25 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <section className="w-screen h-screen bg-black bg-opacity-75 flex items-center justify-center">
-      <form onSubmit={handleSubmit} className="w-full max-w-md w-full max-w-screen-sm md:w-1/2 lg:w-1/3 mx-auto p-8 rounded-md shadow-lg md:mt-0 mt-8 px-4 sm:px-8">
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-                <label
-                className={`block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2 ${
-                    isFocused(nameRef) ? 'text-red-500' : ''
-                }`}
-                htmlFor="name"
-                >
-                Name
-                </label>
-            <input
-              ref={nameRef}
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="name"
-              type="text"
-              name="name"
-              value={formState.name}
-              onChange={handleInputChange}
-              required
-            />
+    <section id="contact" className="w-screen h-screen bg-black bg-opacity-75 text-white flex items-center justify-center">
+      <div className="container mx-auto px-4 md:px-8 max-w-5xl">
+        <h2 className="text-3xl font-bold mb-8">Get in touch</h2>
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="flex flex-col">
+            <label htmlFor="name" className={`mb-2 ${isFocused(nameRef) ? 'text-red-500' : ''}`}>Name</label>
+            <input type="text" id="name" name="name" ref={nameRef} value={formState.name} onChange={handleInputChange} className="border border-gray-400 p-2 rounded-md text-black" required />
           </div>
-          <div className="w-full px-3">
-            <label
-                className={`block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2 ${
-                    isFocused(emailRef) ? 'text-red-500' : ''
-                }`}
-                htmlFor="email"
-                >
-                Email
-            </label>
-            <input
-              ref={emailRef}
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="email"
-              type="email"
-              name="email"
-              value={formState.email}
-              onChange={handleInputChange}
-              required
-            />
+          <div className="flex flex-col">
+            <label htmlFor="email" className={`mb-2 ${isFocused(emailRef) ? 'text-red-500' : ''}`}>Email</label>
+            <input type="email" id="email" name="email" ref={emailRef} value={formState.email} onChange={handleInputChange} className="border border-gray-400 p-2 rounded-md text-black" required />
           </div>
-          <div className="w-full px-3">
-            <label
-                className={`block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2 ${
-                    isFocused(messageRef) ? 'text-red-500' : ''
-                }`}
-                htmlFor="message"
-                >
-                Message
-            </label>
-            <textarea
-              ref={messageRef}
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="message"
-              name="message"
-              rows={4}
-              value={formState.message}
-              onChange={handleInputChange}
-              required
-            ></textarea>
+          <div className="flex flex-col col-span-full">
+            <label htmlFor="message" className={`mb-2 ${isFocused(messageRef) ? 'text-red-500' : ''}`}>Message</label>
+            <textarea id="message" name="message" rows={4} ref={messageRef} value={formState.message} onChange={handleInputChange} className="border border-gray-400 p-2 rounded-md text-black" required></textarea>
           </div>
-          <div className="w-full px-3 mt-4">
-          <button
-              type="submit"
-              className="w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Send
-            </button>
-          </div>
-        </div>
-      </form>
+          <button type="submit" className="bg-red-500 text-white font-bold py-2 px-4 rounded-md col-span-full">Submit</button>
+        </form>
+      </div>
     </section>
   );
 };
--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import styles from './Hero.module.css';
 
 const Hero: React.FC = () => {
   const nameRef = useRef<HTMLHeadingElement>(null);
@@ -47,18 +48,56 @@ const Hero: React.FC = () => {
     };
   }, []);
 
+  const typeWriter = useCallback(() => {
+    const words = ['Full Stack', 'App', 'CLI'];
+    let wordIndex = 0;
+    let isDeleting = false;
+    let txt = '';
+  
+    const typingEffect = () => {
+      const currentWord = words[wordIndex % words.length];
+      txt = isDeleting ? currentWord.substring(0, txt.length - 1) : currentWord.substring(0, txt.length + 1);
+  
+      const typingElement = document.querySelector('.typing-effect');
+      if (typingElement) typingElement.textContent = txt;
+  
+      let typingSpeed = 150 - Math.random() * 100;
+  
+      if (isDeleting) typingSpeed /= 2;
+  
+      if (!isDeleting && txt === currentWord) {
+        typingSpeed = 1600;
+        isDeleting = true;
+      } else if (isDeleting && txt === '') {
+        isDeleting = false;
+        wordIndex++;
+        typingSpeed = 600;
+      }
+  
+      setTimeout(() => typingEffect(), typingSpeed);
+    };
+  
+    typingEffect();
+  }, []);
 
+  useEffect(() => {
+    typeWriter();
+  }, [typeWriter]);
 
   return (
     <section className="w-screen h-screen bg-black bg-opacity-75 flex flex-col items-center justify-center">
-      <div className="text-white text-8xl font-bold text-center md:text-left">
-        <span className="text-6xl md:text-8xl">Hiya, I'm </span>
-        <h1 ref={nameRef} className='text-red-500 px-1 inline font-fira' data-value="Ember Hext">
+      <div className="text-white text-5xl md:text-8xl font-bold text-center md:text-left">
+        <span className="text-4xl md:text-6xl lg:text-8xl">Hiya, I'm </span>
+        <h1 ref={nameRef} className={`text-red-500 px-1 inline font-fira ${styles.magic}`} data-value="Ember Hext">
           Ember Hext
         </h1>
       </div>
-      <div className="text-white text-5xl font-semibold mt-4">Developer</div>
-      <div className="flex space-x-4 mt-4">S
+      <div className="text-white text-3xl md:text-5xl font-semibold mt-4">
+        <span className="typing-effect"></span>
+        <span className={styles['text-cursor']}></span>
+        <span> Developer</span>
+      </div>
+      <div className="flex space-x-2 md:space-x-4 mt-2 md:mt-4">
         <a
           href="https://twitter.com/emberhext"
           target="_blank"
